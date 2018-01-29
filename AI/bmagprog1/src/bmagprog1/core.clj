b30ce379217825e530fa156fbf5572bfd69fbcdd
BenGirone <bgirone001@student.franciscan.edu>
2018-01-29 11:51:55 -0800

    Did parts 1-3 and 5

--- AI/bmagprog1/src/bmagprog1/core.clj
+++ AI/bmagprog1/src/bmagprog1/core.clj
@@ -1,7 +1,29 @@
+;; Ben Girone. 1/29/18. CSC 410.
+;; This program gives examples of many common structures in Clojure.
+
 (ns bmagprog1.core
   (:gen-class))
 
+(defn is-old
+	"A function with one parameter (part 5) that uses 'if' (part 1) and 'do' (part 2) and 'when' (part 3)"
+	[age]
+
+	(if (> age 30)
+		(print "You are old. ")
+		(do (print "You are young. ")
+			(print "You have " (- 30 age) "years until you are old")))
+
+	(when (> age 150)
+		(print "You should see a doctor. ")
+		(print "You might be an immortal porpoise."))
+
+	(println ""))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+	"Tests the various functions."
+	[& args]
+
+	;;test the is-old function
+	(is-old 25)
+	(is-old 60)
+	(is-old 160))
