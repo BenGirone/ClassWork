25cdc1ac6aba229341172fd0326166898fa0ba0e
BenGirone <bgirone001@student.franciscan.edu>
2018-01-31 18:05:19 -0500

    Added more parts

--- AI/bmagprog1/src/bmagprog1/core.clj
+++ AI/bmagprog1/src/bmagprog1/core.clj
@@ -11,13 +11,31 @@
 	(if (> age 30)
 		(print "You are old. ")
 		(do (print "You are young. ")
-			(print "You have " (- 30 age) "years until you are old")))
+			(print "You have" (- 30 age) "years until you are old")))
 
 	(when (> age 150)
 		(print "You should see a doctor. ")
 		(print "You might be an immortal porpoise."))
 
 	(println ""))
+
+(defn zero-param
+	"A function with no parameters (part 4)"
+	[]
+
+	(println "I'm lonely because I have no params."))
+
+(defn multi-arity
+	"A multi-arity function (part 6)"
+	([name] (multi-arity name "a while ago"))
+	([name DoB] 
+		(println name "was born" DoB)))
+
+(defn add-stuff
+"A function with default values (part 7)"
+
+	)
+
 
 (defn -main
 	"Tests the various functions."
@@ -26,4 +44,7 @@
 	;;test the is-old function
 	(is-old 25)
 	(is-old 60)
-	(is-old 160))
+	(is-old 160)
+	(zero-param)
+	(multi-arity "Billy" "October 1st 1999")
+	(multi-arity "Susie"))
