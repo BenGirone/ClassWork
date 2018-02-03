cbf39c2e9a8e45deda1da43fd948d6964e310b12
BenGirone <bgirone001@student.franciscan.edu>
2018-02-02 21:22:24 -0500

    finished prog 1

--- AI/bmagprog1/src/bmagprog1/core.clj
+++ AI/bmagprog1/src/bmagprog1/core.clj
@@ -8,11 +8,13 @@
 	"A function with one parameter (part 5) that uses 'if' (part 1) and 'do' (part 2) and 'when' (part 3)"
 	[age]
 
+	;;test if and do
 	(if (> age 30)
 		(print "You are old. ")
 		(do (print "You are young. ")
 			(print "You have" (- 30 age) "years until you are old")))
 
+	;;test when
 	(when (> age 150)
 		(print "You should see a doctor. ")
 		(print "You might be an immortal porpoise."))
@@ -27,24 +29,34 @@
 
 (defn multi-arity
 	"A multi-arity function (part 6)"
-	([name] (multi-arity name "a while ago"))
-	([name DoB] 
-		(println name "was born" DoB)))
+	([firstName] 
+		(println "Hello" firstName "I wish I knew your last name."))
+	([firstName lastName] 
+		(println "Hello" firstName lastName "I'm glad I know your last name.")))
 
-(defn add-stuff
+(defn liam-neeson
 "A function with default values (part 7)"
+	([] (liam-neeson "find you"))
+	([action1] (liam-neeson action1 "kill you"))
+	([action1 action2] (println "I will" action1 "... and I will" action2)))
 
-	)
-
+(defn add-a-lot
+"A function with a variable parameter list (part 8)"
+	[& numbers]
+	(println (reduce + numbers)))
 
 (defn -main
 	"Tests the various functions."
 	[& args]
 
-	;;test the is-old function
 	(is-old 25)
 	(is-old 60)
 	(is-old 160)
 	(zero-param)
-	(multi-arity "Billy" "October 1st 1999")
-	(multi-arity "Susie"))
+	(multi-arity "Billy" "Smith")
+	(multi-arity "Susie")
+	(liam-neeson)
+	(liam-neeson "find you" "put a smile on your face")
+	(add-a-lot 1 2 3)
+	(add-a-lot 1 2 3 4))
+
