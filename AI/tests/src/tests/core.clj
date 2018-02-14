7b89151154397168cc0548e7ab1eec756906dd7d
BenGirone <bgirone001@student.franciscan.edu>
2018-02-14 15:00:19 -0500

    made a testing file

---
+++ AI/tests/src/tests/core.clj
@@ -0,0 +1,16 @@
+(ns tests.core
+  (:gen-class))
+
+(defn loopTest
+"Takes an array of the form [ [1 2 3] [4 5 6] ] and outputs [1 2 3 4 5 6]"
+	[x]
+	(loop [xLoop x result []]
+		(if (empty? xLoop)
+			result
+			(let [[inner & remaining] xLoop]
+				(recur remaining (into result inner))))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (loopTest [ [1 2 3] [4 6] [9] [1 2 3 4 5] ]))
