a2db7d3f025f4be9ff1a1b900257619d8f91af2b
BenGirone <bgirone001@student.franciscan.edu>
2018-02-28 18:23:21 -0500

    almost done

--- AI/tests/src/tests/core.clj
+++ AI/tests/src/tests/core.clj
@@ -9,3 +9,23 @@
   "I don't do a whole lot ... yet."
   [& args]
   )
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
+
+(defn copy
+	[data]
+	(reduce (fn [copyData item]
+		(into copyData [item item]))
+	[]
+	data))
+
+
+(defn titleize
+  [topic]
+  (str topic " for the Brave and True"))
