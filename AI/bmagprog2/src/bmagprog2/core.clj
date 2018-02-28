f9838e776965864fa996dfb37a1a01f24432b60d
BenGirone <bgirone001@student.franciscan.edu>
2018-02-27 22:30:55 -0500

    finished prog2

--- AI/bmagprog2/src/bmagprog2/core.clj
+++ AI/bmagprog2/src/bmagprog2/core.clj
@@ -1,6 +1,7 @@
 (ns bmagprog2.core
   (:gen-class))
 
+;; 2.
 (defn student-related-details
 	"Accepts the id-number of a student and returns the record details."
 	[id data]
@@ -12,11 +13,13 @@
 					part
 					(recur remaining))))))
 
+;; 3.
 (defn unhinged
 	"Returns true if a student is not fully sane.returns true if a student is not fully sane."
 	[student]
 	(< (:sanity student) 6))
 
+;; 4.
 (defn unstable-students
 	"Returns a list of potential unstable students."
 	[data]
@@ -28,8 +31,10 @@
 					(recur remaining (into unstable [part]))
 					(recur remaining unstable))))))
 
+;; 5.
 (def very-stable #((complement unhinged) %))
 
+;; 6.
 (defn adjusted-sanity-students
 	"Returns a list of all the students who enjoy clojure with their sanity-rating set to -2."
 	[data]
@@ -41,11 +46,44 @@
 					(recur remaining (into adjusted [(assoc part :sanity -2)]))
 					(recur remaining adjusted))))))
 
+;; 7.
+(defn squared-numbers
+  ([] (squared-numbers 1))
+  ([n] (cons (* n n) (lazy-seq (squared-numbers (inc n))))))
+
+;; 8.
+(defn fibonacci
+	"Take a positive integer n and returns the nth Fibonacci number."
+	[n]
+	(if (= n 1) 
+		0
+	(if (= n 2) 
+		1
+		(+ (fibonacci (- n 1)) (fibonacci (- n 2))))))
+
+;; 9.
+(def lazy-fib
+	((fn f [a b] 
+		(lazy-seq (cons a (f b (+ a b)))))
+	0 1))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Tests each part of the assignment"
   [& args]
+
+  ;; 1.
   (def bmg-data [{:id 0 :name "jimmy" :clojure false :sanity 9}
   				 {:id 1 :name "johny" :clojure true  :sanity 2}
   				 {:id 2 :name "jilly" :clojure true  :sanity 5}
   				 {:id 3 :name "janey" :clojure true  :sanity 8}
-  				 {:id 4 :name "jelly" :clojure false :sanity 10}]))
+  				 {:id 4 :name "jelly" :clojure false :sanity 10}])
+
+  (println "2: " (str (student-related-details 1 bmg-data)) "\n")
+  (println "3: " (str (unhinged (student-related-details 2 bmg-data))) "\n")
+  (println "4: " (str (unstable-students bmg-data)) "\n")
+  (println "5: " (str (very-stable (student-related-details 2 bmg-data))) "\n")
+  (println "6: " (str (adjusted-sanity-students bmg-data))  "\n")
+  (println "7: " (clojure.string/join ", " (take 6 (squared-numbers))) "\n")
+  (println "8: " (str (fibonacci 6)) "\n")
+  (println "9: " (clojure.string/join ", " (take 6 lazy-fib)) "\n"))
+  
--- AI/tests/src/tests/core.clj
+++ AI/tests/src/tests/core.clj
@@ -1,16 +1,11 @@
 (ns tests.core
   (:gen-class))
 
-(defn loopTest
-"Takes an array of the form [ [1 2 3] [4 5 6] ] and outputs [1 2 3 4 5 6]"
-	[x]
-	(loop [xLoop x result []]
-		(if (empty? xLoop)
-			result
-			(let [[inner & remaining] xLoop]
-				(recur remaining (into result inner))))))
+(defn destructo
+	[[first second & rest]]
+	second)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (set (loopTest [ [1 2 3] [4 6] [9] [1 2 3 4 5] ])))
+  )
