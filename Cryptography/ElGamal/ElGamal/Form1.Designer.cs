c32ed8f3caaffa915f71b2c305ef718b93c25744
BenGirone <BGirone001@student.franciscan.edu>
2018-03-23 00:23:04 -0400

    Started ElGamal Project

---
+++ Cryptography/ElGamal/ElGamal/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ElGamal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plainText = new System.Windows.Forms.RichTextBox();
+            this.btn_encrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // plainText
+            // 
+            this.plainText.Location = new System.Drawing.Point(93, 12);
+            this.plainText.Name = "plainText";
+            this.plainText.Size = new System.Drawing.Size(699, 499);
+            this.plainText.TabIndex = 0;
+            this.plainText.Text = "";
+            // 
+            // btn_encrypt
+            // 
+            this.btn_encrypt.Location = new System.Drawing.Point(12, 12);
+            this.btn_encrypt.Name = "btn_encrypt";
+            this.btn_encrypt.Size = new System.Drawing.Size(75, 23);
+            this.btn_encrypt.TabIndex = 1;
+            this.btn_encrypt.Text = "Generate";
+            this.btn_encrypt.UseVisualStyleBackColor = true;
+            this.btn_encrypt.Click += new System.EventHandler(this.btn_encrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 523);
+            this.Controls.Add(this.btn_encrypt);
+            this.Controls.Add(this.plainText);
+            this.Name = "Form1";
+            this.Text = "El Gamal Messenger";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox plainText;
+        private System.Windows.Forms.Button btn_encrypt;
+    }
+}
+
---
+++ Cryptography/ElGamal/ElGamal/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace ElGamal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            plainText.Text = "Number : Is Prime\n";
+        }
+
+        private void btn_encrypt_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                BigInteger candidate = BigPrimes.RandomPrimeCandidate(128 * 5);
+                if (BigPrimes.IsProbablePrime(candidate, 20))
+                {
+                    plainText.Text += candidate.ToString() + " : " + BigPrimes.IsProbablePrime(candidate, 20).ToString() + "\n";
+                    break;
+                }
+            }
+        }
+
+        public string listToString(List<BigInteger> L)
+        {
+            string s = "[";
+
+            for (int i = 0; i < L.Count - 1; i++)
+            {
+                s += L[i].ToString() + ", ";
+            }
+
+            s += L[L.Count - 1].ToString() + "]";
+
+            return s;
+        }
+    }
+}
